9e3a45ca04ca5ece56de2b8fbcd91db5dd54a656
Vitaly <vitalage@yandex.ru>
2024-05-04 20:16:19 +0300

    refactor(keyboards.inline): remove non-working 'edit' button, removed unnecessary framworks

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -1,22 +1,9 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-
-from app.database.requests import get_reminders, get_reminder
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 async def actions_reminders(reminder_id):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="Удалить", callback_data=f"delete_reminder_{reminder_id}"),
-         InlineKeyboardButton(text="Изменить", callback_data=f"update_reminder_{reminder_id}")]
+         ]
     ])
     return keyboard
-
-
-# async def reminder(tg_id, reminder_id):
-#     all_reminders = await get_reminders(tg_id)
-#     keyboard = InlineKeyboardBuilder()
-#     for reminder in all_reminders:
-#         keyboard.add(InlineKeyboardButton(text=reminder.text, callback_data=f"reminder_{reminder.id}"))
-#     keyboard.add(InlineKeyboardButton(text="На главную", callback_data='to_main'))
-#     return keyboard.adjust(2).as_markup()
